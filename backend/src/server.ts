22b08f2a9328398dcb2597941bfa428937ef6270
Anderson <andersonlimacrv@gmail.com>
2024-10-08 17:23:23 -0300

    feat: remove product example.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,10 @@
 import { ApiExpress } from "./api/express/api.express";
-import { ProductController } from "./api/express/controllers/product.controller";
 import { AlunoController } from "./api/express/controllers/aluno.controller";
 
 function main(){
   const api = ApiExpress.build();
 
-  const productController = ProductController.build();
   const alunoController = AlunoController.build();
-
-  api.addGetRoute("/products", productController.list);
-  api.addPostRoute("/product/buy", productController.buy);
-  api.addPostRoute("/product/sell", productController.sell);
-  api.addPostRoute("/product/create", productController.create);
 
   api.addGetRoute("/alunos", alunoController.list);
   api.addPostRoute("/aluno/create", alunoController.create);
